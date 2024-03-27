9ac93fad9bc015133852c284cd948d7e6868cd6c
Taylor Jones <9523996+TaylorJonesTRT@users.noreply.github.com>
2024-03-26 22:01:12 -0500

    Finished workout edit page, made enhancements to other exisiting pages

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,44 +1,26 @@
+require 'pp'
+
 class WorkoutsController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
 
   def show
-    @workout = current_user.workouts.find_by(id: params[:id])
-    @workout_trainings = @workout.trainings.all
-
-    workout_hash = {
-      'id' => @workout.id,
-      'created_at' => @workout.created_at,
-      'user_id' => @workout.user_id,
-      'trainings' => []
-    }
-
-    @workout_trainings.each do |training|
-      hash = {
-        'name' => nil,
-        'training_id' => nil,
-        'exercise_id' => nil,
-        'rep_sets' => nil
-      }
-      training_rep_sets = @workout.rep_sets.all.select { |m| m.training_id == training.id }
-      puts training_rep_sets
-      exercise = Exercise.find_by(id: training.exercise_id)
-      hash['rep_sets'] = training_rep_sets
-      hash['training_id'] = training.id
-      hash['name'] = exercise.name
-      hash['exercise_id'] = exercise.id
-      workout_hash['trainings'].push(hash)
-    end
+    @workout = current_user.workouts.find(params[:id])
+    @trainings = Training.where(workout_id: @workout).all
   end
 
   def new
     @workout = current_user.workouts.build
   end
 
+  def edit
+    @workout = Workout.find(params[:id])
+    @workout_trainings = @workout.trainings.order(id: :asc)
+  end
+
   def create
     @workout = current_user.workouts.build(user_id: current_user.id)
-
     if @workout.save
       workout_params[:exercises].each do |exercise|
         @training = Training.create(workout_id: @workout.id, exercise_id: exercise)
@@ -53,11 +35,6 @@ class WorkoutsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def edit
-    @workout = Workout.find(params[:id])
-    @workout_trainings = @workout.trainings.order(id: :asc)
   end
 
   def update; end
