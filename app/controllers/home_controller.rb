9d868cb6291911af8fc60ce50132e426bf53fac0
Taylor Jones <9523996+TaylorJonesTRT@users.noreply.github.com>
2024-04-06 18:08:46 -0500

    Fleshed out workout list partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
-require 'pp'
-
 class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
     @recent_workouts = current_user.workouts.order(id: :asc).last(3)
-    pp @recent_workouts
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,16 @@
 module HomeHelper
+  def format_exercise_names(workout)
+    exercise_names = []
+    trainings = Workout.find(workout.id).trainings
+
+    trainings.each do |training|
+      exercise_names << Exercise.find_by(id: training.exercise_id).name
+    end
+
+    # exercise_names = []
+    # exercises.each do |exercise|
+    #   exercise_names << exercise.name
+    # end
+    exercise_names.join(', ')
+  end
 end
