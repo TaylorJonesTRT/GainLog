402101d90c25b3c204c7a0e04c8184ea5cbef23a
Taylor Jones <9523996+TaylorJonesTRT@users.noreply.github.com>
2024-04-09 16:28:21 -0500

    another config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.serve_statice_assets = false
+  config.serve_statice_assets = true
   config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
